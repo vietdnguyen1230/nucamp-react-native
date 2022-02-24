e369f61db5b2f2b49fbbef2032144576cae102ef
Viet Nguyen <vietnguyen2330@gmail.com>
2022-02-24 07:34:14 -0800

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
-import { ScrollView, Text } from "react-native";
-import { Card } from 'react-native-elements';
-import { FlatList } from 'react-native';
-import { PARTNERS } from '../shared/partners';
+import React, { Component } from 'react';
+import { ScrollView, Text, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
     return(
@@ -17,14 +22,6 @@ function Mission() {
 }
 class About extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        };
-    }
-
-
     static navigationOptions = {
         title: 'About Us'
     }
@@ -36,7 +33,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         };
@@ -46,8 +43,8 @@ class About extends Component {
                 <Mission />
                 <Card
                     title="Community Partners">
-                    <FlatList
-                        data={this.state.partners}
+                    <FlatList 
+                        data={this.props.partners.partners}
                         renderItem={renderPartner}
                         keyExtractor={item => item.id.toString()}
                     />
@@ -57,4 +54,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
