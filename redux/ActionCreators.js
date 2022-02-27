f7b83cce0be28e528ec658d4a0d7c7da46206173
Viet Nguyen <vietnguyen2330@gmail.com>
2022-02-27 12:32:10 -0800

    Week 2 Workship Version 1.0

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -141,3 +141,33 @@ export const addPartners = partners => ({
     type: ActionTypes.ADD_PARTNERS,
     payload: partners
 });
+
+export const postFavorite = campsiteId => dispatch => {
+    setTimeout(() => {
+        dispatch(addFavorite(campsiteId));
+    }, 2000);
+};
+
+export const addFavorite = campsiteId => ({
+    type: ActionTypes.ADD_FAVORITE,
+    payload: campsiteId
+});
+
+export const postComment = ( campsiteId, rating, author, text ) => dispatch => {
+    const newComment = {
+        campsiteId,
+        rating,
+        author,
+        text
+    };
+    newComment.date = new Date().toISOString();
+
+    setTimeout(() => {
+        dispatch(addComment(newComment));
+    }, 2000);
+};
+
+export const addComment = comment => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
+});
