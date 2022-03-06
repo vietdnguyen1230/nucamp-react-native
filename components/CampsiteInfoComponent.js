4f4286129cac0b3a16f0f57c072b01efa35a8cda
Viet Nguyen <vietnguyen2330@gmail.com>
2022-03-05 17:33:06 -0800

    Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,8 +26,14 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const view = React.createRef();
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
@@ -58,9 +64,10 @@ function RenderCampsite(props) {
             <Animatable.View 
                 animation='fadeInDown' 
                 duration={2000} 
-                delay={1000} 
+                delay={1000}
+                ref={view} 
                 {...panResponder.panHandlers}>
-                    
+
                 <Card
                     featuredTitle={campsite.name}
                     image={{uri: baseUrl + campsite.image}}>
